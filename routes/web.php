926f67e05d30a27093778c0a118036c7aa0cbbe8
Ali-Al-Noufal <alialnoufal3@gmail.com>
2026-02-04 17:45:44 -0500

    finish

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/seed-data', function () {
+    try {
+        Artisan::call('db:seed', ['--force' => true]);
+        return "تمت إضافة البيانات بنجاح!";
+    } catch (\Exception $e) {
+        return $e->getMessage();
+    }
 });
